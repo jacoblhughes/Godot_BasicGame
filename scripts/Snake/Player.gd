7ad34119b6d9cde2abc833da778f5139f8867898
Jacob <hughes.jacobl@gmail.com>
2023-09-26 13:57:40 -0700

    started snake

--- scripts/Snake/Player.gd
+++ scripts/Snake/Player.gd
@@ -4,10 +4,17 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 var velocity = Vector2.ZERO # The player's movement vector.
 
+var tile_size = 0
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+
 func _ready():
 	screen_size = get_parent().get_node("Play_Area").size
-	pass # Replace with function body.
-
+	tile_size = screen_size.x/64
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -28,10 +35,13 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$Animation.play()
+
 	else:
-		$Animation.stop()
+		pass
 	
-	position += velocity * delta
+	position += velocity * (tile_size/128)
 	position = position.clamp(Vector2.ZERO, screen_size)
 	pass
+
+func _input(event):
+	print(event)
