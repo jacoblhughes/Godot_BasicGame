6bb9a10c06e56c7b7f04b8ad24f39ce5f02a8704
Jacob Hughes <hughes.jacobl@gmail.com>
2023-09-28 19:57:44 -0700

    more variables

--- data/Snake/singleton/SnakeVariables.gd
+++ data/Snake/singleton/SnakeVariables.gd
@@ -1,15 +1,20 @@
 extends Node
 
-var snakecells = 64
-@onready var PLAY_AREA : ColorRect
-@onready var GRID_SIZE := Vector2(0,0)
+var snakecells = 8
 
+var GRID_SIZE := Vector2(0,0)
+var GRID_POSITION := Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	PLAY_AREA = get_tree().get_root().get_node("GameScene").get_node("Snake").get_node("Node2d").get_node("Play_Area")
-	GRID_SIZE = Vector2(PLAY_AREA.x,PLAY_AREA.y)
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
+
+func _set_play_area_size(item):
+	GRID_SIZE = Vector2(item.x,item.y)
+	
+func _set_play_area_position(item):
+	GRID_POSITION = Vector2(item.x,item.y)
--- scripts/Snake/grid.gd
+++ scripts/Snake/grid.gd
@@ -1,31 +1,32 @@
 extends Node2D
 
-@onready var width = $Play_Area.size.x
-@onready var height = $Play_Area.size.y
-
-@onready var cells =8
-
-@onready var sizewidth = width/cells
-@onready var sizeheight = height/cells
-
-var originalx = 40
-var originaly = 160
+var width
+var height
+var sizewidth 
+var sizeheight
+var originalx
+var originaly
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(width)
-	print(height)
-	print(SnakeVariables.snakecells)
+	SnakeVariables._set_play_area_size($Play_Area.size)
+	SnakeVariables._set_play_area_position($Play_Area.position)
+	sizewidth = SnakeVariables.GRID_SIZE.x/SnakeVariables.snakecells
+	sizeheight = SnakeVariables.GRID_SIZE.y/SnakeVariables.snakecells
+	width = SnakeVariables.GRID_SIZE.x
+	height = SnakeVariables.GRID_SIZE.y
+	originalx = SnakeVariables.GRID_POSITION.x
+	originaly = SnakeVariables.GRID_POSITION.y
 	pass
 	
 func _draw():
 	
 
-	for i in cells+1:
+	for i in SnakeVariables.snakecells+1:
 
 		var vectortest = Vector2(i*sizewidth+originalx,0+originaly)
 		var vectortest1 = Vector2(i*sizewidth+originalx,height+originaly)
 		draw_line(vectortest,vectortest1,'white')
-	for i in cells+1:
+	for i in SnakeVariables.snakecells+1:
 		var vectortest2 = Vector2(0+originalx,i*sizeheight+originaly)
 		var vectortest3 = Vector2(width+originalx,i*sizeheight+originaly)
 		draw_line(vectortest2,vectortest3,'white')
