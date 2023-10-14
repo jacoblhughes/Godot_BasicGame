6418e350094ce3c0fe504e1fe3b4d4c94e8dc06e
Jacob Hughes <hughes.jacobl@gmail.com>
2023-10-14 10:54:20 -0700

    more dino

--- scripts/Dino/dino.gd
+++ scripts/Dino/dino.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@onready var HUDSIGNALS = get_tree().get_root().get_node("Main").get_node("HUD_SCENE")
 @onready var enemy_scene  = preload("res://scenes/Dino/Enemy.tscn")
 var enemy_counter = 1
 
@@ -29,6 +29,6 @@ func _on_enemy_spawn_timer_timeout():
 func _on_despawn_body_entered(body):
 
 	if("Enemy" in body.name):
-
 		body.queue_free()
+		HUDVariables.set_new_score(1)
 	pass # Replace with function body.
