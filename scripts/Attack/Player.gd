c7b8b95ea85498a3356011f38e94184326046170
Jacob <hughes.jacobl@gmail.com>
2023-10-17 19:46:02 -0700

    more attack

--- scripts/Attack/Player.gd
+++ scripts/Attack/Player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,20 +8,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	velocity=Vector2(0,0)
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if Input.is_action_pressed("move_right"):
+		velocity.x = SPEED
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -SPEED
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -SPEED
+	if Input.is_action_pressed("move_down"):
+		velocity.y = SPEED
 	move_and_slide()
