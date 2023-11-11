c824bafc925043ef8b135519db9d7dcc8821bde7
Jacob Hughes <hughes.jacobl@gmail.com>
2023-11-11 09:23:56 -0800

    more

--- scripts/Dino/enemy.gd
+++ scripts/Dino/enemy.gd
@@ -3,14 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 signal player_collision
-var book_texture = preload("res://art/Dino/bookcase.png")
-var carrot_texture = preload("res://art/Dino/carrot.png")
-var red_flower_texture = preload("res://art/Dino/Enemy Top Red.png")
-var blue_flower_textyre = preload("res://art/Dino/Enemy Top Blue.png")
-var yelllow_flower_texture = preload("res://art/Dino/Enemy Top Yellow.png")
+var book_texture = preload("res://art/Dino/bookcase_256.png")
+var carrot_texture = preload("res://art/Dino/carrot_256.png")
+var clock_texture = preload("res://art/Dino/clock_256.png")
+var cactus_texture = preload("res://art/Dino/cactus_256.png")
+var pizza_texture = preload("res://art/Dino/pizza_256.png")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var all_textures = [book_texture, carrot_texture,red_flower_texture,blue_flower_textyre,yelllow_flower_texture] # ... add other textures
+var all_textures = [book_texture, carrot_texture,clock_texture,cactus_texture,pizza_texture] # ... add other textures
 
 func _ready():
 	var chosen_texture = all_textures[randi() % all_textures.size()]
