bbfb2b5b6087ff9b8d620162b5ab67d4295a32f1
Jacob <hughes.jacobl@gmail.com>
2023-11-18 10:12:49 -0800

    more

--- scripts/Dino/player.gd
+++ scripts/Dino/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+var double_jump_counter = false
 const JUMP_VELOCITY = -600.0
 var rng = RandomNumberGenerator.new()
 var animation_player: AnimatedSprite2D
@@ -17,6 +18,7 @@ func _ready():
 
 func _physics_process(delta):
 	if(GameManager.get_game_enabled()):
+		
 
 		if not is_on_floor():
 			animation_player.play("jumping")
@@ -24,25 +26,14 @@ func _physics_process(delta):
 			
 		if is_on_floor():
 			animation_player.play("default")  # Start the "jumping" animation
+			double_jump_counter = false
 
 		
-		# Handle Jump.
-		if Input.is_action_just_pressed("left_mouse_click") and is_on_floor() and rng.randi_range(1,100) == 69:
-			velocity.y = JUMP_VELOCITY * 10
-		elif Input.is_action_just_pressed("left_mouse_click") and is_on_floor():
+		if Input.is_action_just_pressed("left_mouse_click") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
-
-#	if direction and direction == -1 :
-#		velocity.x = direction * SPEED
-##		animation_player.play("walking")  # Start the "jumping" animation
-#		animation_player.flip_h=true
-#	elif direction and direction == 1 :
-#		velocity.x = direction * SPEED
-##		animation_player.play("walking")  # Start the "jumping" animation
-#		animation_player.flip_h=false
-#
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
+		elif Input.is_action_just_pressed("left_mouse_click") and not is_on_floor() and double_jump_counter  == false:
+			velocity.y = JUMP_VELOCITY
+			double_jump_counter = true
 
 	move_and_slide()
 	for i in range(get_slide_collision_count()):
@@ -51,8 +42,24 @@ func _physics_process(delta):
 		if("Enemy" in collision.get_collider().name):
 			self.collision_mask = 0  # This will disable the player's ability to detect enemies (or anything else, for that matter).
 			collision.get_collider().queue_free()
-
+			print('game_over')
 			dino_hit.emit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
