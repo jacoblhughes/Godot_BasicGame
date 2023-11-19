7de8c312022662692a8cbadb47c187c5ff25e3a5
Jacob <hughes.jacobl@gmail.com>
2023-11-19 12:24:55 -0800

    mormoremormore

--- scripts/Main/main.gd
+++ scripts/Main/main.gd
@@ -4,7 +4,9 @@ signal main_ready
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	DisplayServer.window_set_size(Vector2i(1080,1920))
 	main_ready.emit()
+
 	pass # Replace with function body.
 
 			
