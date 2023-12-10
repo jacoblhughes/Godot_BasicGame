7a7a3710be689823d9936d548996856a4e705d6d
Jacob <hughes.jacobl@gmail.com>
2023-12-10 13:45:45 -0800

    more

--- project/src/perry_llama/enemy.gd
+++ project/src/perry_llama/enemy.gd
@@ -3,18 +3,29 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 signal player_collision
-var book_texture = preload("res://textures/perry_llama/bookcase_256.png")
-var carrot_texture = preload("res://textures/perry_llama/carrot_256.png")
+var normal_texture = preload("res://textures/perry_llama/bookcase_256.png")
+var wide_texture = preload("res://textures/perry_llama/carrot_256.png")
 var clock_texture = preload("res://textures/perry_llama/clock_256.png")
 var cactus_texture = preload("res://textures/perry_llama/cactus_256.png")
 var pizza_texture = preload("res://textures/perry_llama/pizza_256.png")
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var all_textures = [book_texture, carrot_texture,clock_texture,cactus_texture,pizza_texture] # ... add other textures
 
 func _ready():
-	var chosen_texture = all_textures[randi() % all_textures.size()]
-	$Sprite2D.texture = chosen_texture
+	randomize()
+	var size = randf_range(1, 3)  # Random duration between 1 and 10 seconds
+	var image_number = randf_range(1, 4)
+	if size ==1:
+		scale = Vector2(1,1)
+	elif size == 2:
+		scale  = Vector2(2,1)
+		
+	elif size ==3:
+		scale  = Vector2(1,2)
+		
+		$Sprite2D.global_position += $Sprite2D.height/2
 
 func _physics_process(_delta):
 	velocity.x = -1 * SPEED
--- project/src/perry_llama/perry_llama.gd
+++ project/src/perry_llama/perry_llama.gd
@@ -9,6 +9,7 @@ var level_value = 1
 @onready var enemy_spawner : Node2D
 @onready var start_position : Vector2
 @onready var enemy_position : Vector2
+@onready var despawn : Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	start_position = get_parent().get_node("StartPosition").global_position
@@ -18,6 +19,8 @@ func _ready():
 	enemy_position = enemy_spawner.get_node("EnemyPosition").global_position
 	_game_initialize()
 	player.dino_hit.connect(_on_dino_hit)
+	despawn = get_parent().get_node("Despawn")
+	despawn.body_entered.connect(_on_despawn_body_entered)
 	pass # Replace with function body.
 
 func _game_initialize():
@@ -29,7 +32,6 @@ func _game_initialize():
 	
 func _on_enemy_spawn_timer_timeout():
 	var enemy = enemy_scene.instantiate()
-	enemy.global_position = enemy_position
 	enemy_spawner.add_child.call_deferred(enemy,true)
 	pass # Replace with function body.
 
