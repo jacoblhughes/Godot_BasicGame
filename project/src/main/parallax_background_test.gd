08428ad3139fe83d7b9d36d22d7f3634d53ec8fe
Jacob <hughes.jacobl@gmail.com>
2024-01-20 12:45:14 -0800

    working on positioning

--- project/src/main/parallax_background_test.gd
+++ project/src/main/parallax_background_test.gd
@@ -1,24 +1,22 @@
-extends ParallaxBackground
+extends Node2D
 
 @export var texture: CompressedTexture2D
 
-@onready var parallax : ParallaxLayer = $ParallaxLayer
-@onready var sprite = $ParallaxLayer/Sprite2D
+@onready var parallax = $ParallaxBackground
+@onready var sprite = $ParallaxBackground/ParallaxLayer/Sprite2D
 
 @export var speed = 10
 var scroll = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	sprite.texture = texture
-	print(GameManager.get_play_area_position_from_HUD())
-	sprite.global_position = GameManager.get_play_area_position_from_HUD()
+	sprite.texture = texture
+#	offset = GameManager.get_play_area_position_from_HUD()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-#	parallax.motion_offset.x -= speed * delta
-	print(sprite.global_position)
+	parallax.scroll_offset.x -= speed * delta
 #	scroll = speed * delta
 #	parallax.set_motion_offset(Vector2(scroll,0))
 
