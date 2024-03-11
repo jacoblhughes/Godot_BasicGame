33aa5fc805cbc431a0ad97e108938311e20fb1af
Jacob Hughes <hughes.jacobl@gmail.com>
2024-03-10 19:37:49 -0700

    more

--- project/data/main/GameManager.gd
+++ project/data/main/GameManager.gd
@@ -64,7 +64,7 @@ var games_list : Dictionary = {
 func _ready():
 	
 	main_node = get_tree().get_root().get_node("Main")
-	aspect_ratio_container = main_node.get_node("CenterContainer").get_node("AspectRatioContainer")
+	aspect_ratio_container = main_node.get_node("AspectRatioContainer")
 	buttons = aspect_ratio_container.get_node("Buttons")
 
 
--- project/src/main/main.gd
+++ project/src/main/main.gd
@@ -1,7 +1,9 @@
 extends Control
 
 signal main_ready
-
+@onready var background : ColorRect = %Background
+@onready var aspect_ratio_container = %AspectRatioContainer
+@onready var center_container = %CenterContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
