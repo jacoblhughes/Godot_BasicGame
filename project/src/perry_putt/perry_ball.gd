9f7314cfcf93195b099218647b943f63894e182e
Jacob Hughes <hughes.jacobl@gmail.com>
2024-03-20 22:17:57 -0700

    more

--- project/src/perry_putt/perry_ball.gd
+++ project/src/perry_putt/perry_ball.gd
@@ -1,14 +1,26 @@
 extends RigidBody2D
 
-
+#@export var clikcable_area : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(2).timeout
-	apply_impulse(Vector2(200,170)*5)
+#	%ClickableArea.clickable_input_event.connect(_on_clickable_input)
+#	apply_impulse(Vector2(200,170)*5)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func hide_arrow():
+	%Aim.visible = false
+
+func show_arrow():
+	%Aim.visible = true
+
+func _on_clickable_input(event,input_position):
+	if GameManager.get_game_enabled():
+		if event is InputEventMouseButton:
+			var direction = (input_position - global_position).normalized()
+			rotation = atan2(direction.y, direction.x)
--- project/src/perry_putt/perry_putt.gd
+++ project/src/perry_putt/perry_putt.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-	pass # Replace with function body.
-
+	
+	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+
 	if Input.is_action_just_pressed("hit_space") and !%HitMeter.meter_status:
 
 		%HitMeter.start_meter()
