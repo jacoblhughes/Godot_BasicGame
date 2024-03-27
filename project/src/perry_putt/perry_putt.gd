42b27c8d3efa147fcf7bf1d71c378294897e866d
Jacob Hughes <hughes.jacobl@gmail.com>
2024-03-26 18:02:08 -0700

    more

--- project/src/perry_putt/perry_putt.gd
+++ project/src/perry_putt/perry_putt.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var level_2 : PackedScene
 @export var level_3 : PackedScene
 var initial_score = 100
+var current_scene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	%Hole.ball_sank.connect(_on_perry_ball_sank)
@@ -34,13 +35,19 @@ func _game_over():
 	GameManager.check_highscore_and_rank()
 	
 func apply_level(scene):
+	current_scene = scene
 	var scene_instance = scene.instantiate()
 	scene_instance.ball_sank.connect(_on_ball_sank)
 	scene_instance.game_over.connect(_on_game_over)
 	get_parent().add_child.call_deferred(scene_instance)
 	
 func _on_ball_sank():
-	
+	if current_scene == level_1:
+		apply_level(level_2)
+	if current_scene == level_2:
+		apply_level(level_3)
+	if current_scene == level_3:
+		_game_over()
 	pass
 	
 func _on_game_over():
