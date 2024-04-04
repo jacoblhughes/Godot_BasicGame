528a9bd63d0410b8c8a0192460bfbc51e8fa5baa
Jacob Hughes <hughes.jacobl@gmail.com>
2024-04-04 16:27:06 -0700

    more

--- project/src/perry_skeeball/skeeball.gd
+++ project/src/perry_skeeball/skeeball.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 var manual_move=false
 var move_to_position
+@export var waiting_path : Path3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,14 +14,22 @@ func _process(delta):
 
 func _integrate_forces(state):
 	if manual_move:
-		state.transform.origin =  move_to_position
-		manual_move = false
-		if state.transform.origin.distance_to(move_to_position) < .01:
-			freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
+		var target_position = waiting_path.return_progress_ratio_position()
+		var current_transform = state.get_transform()
+		current_transform.origin = target_position
+		state.set_transform(current_transform)
+#	print(state.step)
+#	if manual_move:
+#		state.transform.origin =  move_to_position
+#		manual_move = false
+#		if state.transform.origin.distance_to(move_to_position) < .01:
+#			freeze_mode = RigidBody3D.FREEZE_MODE_STATIC
 
 func get_ready(input_position):
+	print('here')
 	manual_move = true
-	freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+#	freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+	set_use_custom_integrator(true)
 #	freeze= true
 	move_to_position = input_position
 	pass
