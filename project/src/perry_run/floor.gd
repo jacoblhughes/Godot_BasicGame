2acbb119eb40e97fca406e35567ba582441f10e3
Jacob Hughes <hughes.jacobl@gmail.com>
2024-04-19 16:46:10 -0700

    more

--- project/src/perry_run/floor.gd
+++ project/src/perry_run/floor.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends RigidBody2D
 class_name PerryRunFloor
 
 const SPEED = 300.0
@@ -10,4 +10,4 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	var direction = -1
-
+	linear_velocity.x = SPEED * direction
