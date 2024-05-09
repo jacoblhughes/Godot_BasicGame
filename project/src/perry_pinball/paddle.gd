fbdd64b4be06a72a2f5859ed5340bc4e6dc40c3f
Jacob Hughes <hughes.jacobl@gmail.com>
2024-05-08 18:18:50 -0700

    more

--- project/src/perry_pinball/paddle.gd
+++ project/src/perry_pinball/paddle.gd
@@ -1,42 +1,41 @@
 extends RigidBody2D
-@export var left = true
-var factor
 
+@export var left = true
+var factor = 1
+var flipper_active = false  # Ensure a defined initial state
+var torque_strength = 50000000  # Consider tuning this value based on testing
+var intermediate_time = 0
+var snap_time = .20
+var snap_angle = 50
 func _ready():
-	if left:
-		factor = 1
-	else:
-		factor = -1
+	factor = -1 if not left else 1
 	HUD.clickable_input_event.connect(_on_clickable_input_event)
-	pass # Replace with function body.
+	if left:
+		factor = -1
+		#snap_angle = -abs(snap_angle)  # Ensure left flipper snap angle is negative
+	else:
+		factor = 1
+		#snap_angle = abs(snap_angle)  # Ensure right flipper snap angle is positive
+
 
 func _on_clickable_input_event(event, _input_position):
 	if GameManager.get_game_enabled():
-		if event.pressed:
-			apply_torque_impulse(200000 * -factor)
-		if !event.pressed:
-			apply_torque_impulse(200000 * factor)
-
-		#if event.pressed:
-			#apply_impulse(Vector2(0,2000 * -factor),%Marker2D.global_position)
-		#if !event.pressed:
-			#apply_impulse(Vector2(0,2000 * factor),%Marker2D.global_position)
-
-
-func _physics_process(delta):
-	pass
-	#if Input.is_action_just_pressed("hit_space"):
-		#apply_impulse(Vector2(0,2000 * -factor),%Marker2D.global_position)
-	#elif Input.is_action_just_released("hit_space"):
-		#apply_impulse(Vector2(0,2000 * factor),%Marker2D.global_position)
-
+		flipper_active = event.pressed
 
 func _integrate_forces(state):
-	var current_rotation_degrees = rad_to_deg(rotation)
-	var new_rotation_degrees  # Convert current rotation from radians to degrees
-	if left:
-		new_rotation_degrees = clamp(current_rotation_degrees, 60, 120)
+	#var current_rotation_degrees = rad_to_deg(rotation)
+	#if left:
+		#current_rotation_degrees = clamp(current_rotation_degrees, 60, 120)
+	#else:
+		#current_rotation_degrees = clamp(current_rotation_degrees, -30, 30)
+
+	if flipper_active:
+		# Increment the time towards full extension
+		intermediate_time = min(intermediate_time + state.step, snap_time)
+		# Apply rotation based on the proportion of snap_time elapsed
+		rotation_degrees = (intermediate_time / snap_time) * (snap_angle * factor)
 	else:
-		new_rotation_degrees = clamp(current_rotation_degrees, -30, 30)
-#
-	rotation = deg_to_rad(new_rotation_degrees)  # Convert back to radians for the physics engine
+		# Decrement the time back towards the rest position
+		intermediate_time = max(intermediate_time - state.step, 0)
+		# Apply rotation in reverse based on the proportion of snap_time remaining
+		rotation_degrees = (intermediate_time / snap_time) * (snap_angle * factor)
