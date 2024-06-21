e0937f8837a334eb2b1da0d9f5906ada9a7d5aa3
Jacob Hughes <hughes.jacobl@gmail.com>
2024-06-20 17:44:22 -0700

    more

--- project/src/perry_memory/cards.gd
+++ project/src/perry_memory/cards.gd
@@ -7,7 +7,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var sprite_frames = SpriteFrames.new()
+
 	var animation_name = "default"
 	var texture_width = drawings_png.get_width()
 	var texture_height = drawings_png.get_height()
@@ -16,16 +16,16 @@ func _ready():
 	var cards_vertical = texture_height / card_height
 
 	for v in range(cards_vertical):
+		var sprite_frames = SpriteFrames.new()
 		var matching_card = matching_card_scene.instantiate()
 		for h in range(cards_horizontal):
 			var texture = AtlasTexture.new()
 			texture.atlas = drawings_png
 			var region = Rect2(Vector2(h * card_width, v * card_height), Vector2(card_width, card_height))
-			print(region)
 			texture.region = region
-			sprite_frames.add_frame(animation_name, texture)
-			print("Frame added: ", h + v * cards_horizontal)
+			sprite_frames.add_frame(animation_name, texture,1.0,-1)
 		matching_card.drawings_animation.sprite_frames = sprite_frames
+		matching_card.position += Vector2(50,50) * v
 		add_child(matching_card)
 	pass # Replace with function body.
 
--- project/src/perry_memory/matching_card.gd
+++ project/src/perry_memory/matching_card.gd
@@ -1,21 +1,15 @@
 extends Node2D
 
-var drawings_animation : AnimatedSprite2D
-var selection = 1
-var pixel_region = Rect2(Vector2(0,0),Vector2(32,48))
-# Called when the node enters the scene tree for the first time.
-# Replace with function body.
-const HFRAMES = 17
-const VFRAMES = 12
-const ANIMATION_LENGTH = 17
+@export var drawings_animation : AnimatedSprite2D
+@export var card_animation : AnimatedSprite2D
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
 func _ready():
+	drawings_animation.play("default")
+	card_animation.play("default")
 	pass
 	#var sprite_frames = SpriteFrames.new()
 	#var animation_name = "default"
