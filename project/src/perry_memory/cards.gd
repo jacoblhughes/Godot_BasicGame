c763969efecb8ae1ac9dba0365817630a73c5513
Jacob Hughes <hughes.jacobl@gmail.com>
2024-06-20 21:09:13 -0700

    more

--- project/src/perry_memory/cards.gd
+++ project/src/perry_memory/cards.gd
@@ -14,8 +14,18 @@ func _ready():
 
 	var cards_horizontal = texture_width / card_width
 	var cards_vertical = texture_height / card_height
+	var cards_per_row = 4
+	var rows = 3
+	var viewport_size = get_viewport_rect().size
+	var horizontal_spacing = (viewport_size.x - (cards_per_row * card_width)) / (cards_per_row + 1)
+	var vertical_spacing = card_height*2  # Adjust as needed for desired vertical spacing
 
 	for v in range(cards_vertical):
+		var row = int(v / cards_per_row)
+		var column = v % cards_per_row
+
+		var x = horizontal_spacing * (column + 1) + card_width * column
+		var y = (viewport_size.y / 2) + (row - 1) * (card_height + vertical_spacing)
 		var sprite_frames = SpriteFrames.new()
 		var matching_card = matching_card_scene.instantiate()
 		for h in range(cards_horizontal):
@@ -24,8 +34,10 @@ func _ready():
 			var region = Rect2(Vector2(h * card_width, v * card_height), Vector2(card_width, card_height))
 			texture.region = region
 			sprite_frames.add_frame(animation_name, texture,1.0,-1)
+			sprite_frames.set_animation_loop(animation_name,false)
+			sprite_frames.set_animation_speed(animation_name,30)
 		matching_card.drawings_animation.sprite_frames = sprite_frames
-		matching_card.position += Vector2(50,50) * v
+		matching_card.position = Vector2(x,y)
 		add_child(matching_card)
 	pass # Replace with function body.
 
--- project/src/perry_memory/matching_card.gd
+++ project/src/perry_memory/matching_card.gd
@@ -2,16 +2,28 @@ extends Area2D
 
 @export var drawings_animation : AnimatedSprite2D
 @export var card_animation : AnimatedSprite2D
-
-
+var timer : Timer
+var selected : bool
 
 func _ready():
-	drawings_animation.autoplay = false
-	card_animation.autoplay = false
+	timer = Timer.new()
+	timer.wait_time = 2.0
+	timer.timeout.connect(_on_timer_timeout)
+	add_child(timer)
+	card_animation.animation_finished.connect(_on_card_animation_finished)
 	pass
 
-func _input(event):
-
+func _input_event(viewport, event, shape_idx):
+	selected = true
 	if event is InputEventMouseButton and event.button_index == 1 and event.pressed:
 		drawings_animation.play("default")
 		card_animation.play("default")
+
+
+func _on_timer_timeout():
+	if selected:
+		drawings_animation.play_backwards("default")
+		card_animation.play_backwards("default")
+		selected = false
+func _on_card_animation_finished():
+	timer.start()
