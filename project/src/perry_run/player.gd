2e6c6fce7123634530a45baab696673d6698cf01
Jacob Hughes <hughes.jacobl@gmail.com>
2024-07-16 17:31:09 -0700

    more

--- project/src/perry_run/player.gd
+++ project/src/perry_run/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 class_name PerryRunPlayer
 
 const JUMP_VELOCITY = 800.0
-const MAX_FLY_TIME = .3
+const MAX_FLY_TIME = 2
 var base_gravity :int = 3500
 var gravity : int
 var fly_time = 0.0
@@ -10,6 +10,9 @@ var xpos
 var should_jump
 var jump_scale
 var flying : bool
+
+@export var player_animation : AnimatedSprite2D
+
 @export var jump_height: float
 @export var jump_time_to_peak : float
 @export var jump_time_to_descent : float
@@ -21,7 +24,7 @@ var flying : bool
 
 func _ready():
 	HUD.clickable_input_event.connect(_on_clickable_input_event)
-	
+
 	gravity = base_gravity
 	pass
 
@@ -33,9 +36,10 @@ func _on_clickable_input_event(event, input_position):
 
 func _physics_process(delta):
 	velocity.x = 0
-	
+
 	if is_on_floor():
 		fly_time = 0.0
+		player_animation.play("default")
 
 	if not is_on_floor() and motion_mode == 0:
 		velocity.y += get_gravity() * delta
@@ -43,23 +47,26 @@ func _physics_process(delta):
 	if should_jump and is_on_floor():
 		AudioManager.play_sound("perry_run_jump")
 		jump()
+		player_animation.play("jumping")
 
-	if should_jump and not is_on_floor() and fly_time < MAX_FLY_TIME and GameManager.get_game_enabled() and motion_mode == 0:
-		print('flying')
+	if velocity.y > 0.0 and should_jump and fly_time < MAX_FLY_TIME and GameManager.get_game_enabled() and motion_mode == 0:
 		flying = true
 		fly_time += delta
-	else:
+		player_animation.play("flying")s
+	elif velocity.y > 0.0:
 		flying = false
-		
+		player_animation.play("falling")
+
+	print(flying)
 	move_and_slide()
 	position.x = clamp(position.x, xpos, xpos)  # Keep the player at xpos
-	
+
 func get_gravity() -> float:
-	return jump_gravity if velocity.y < 0.0 else (fall_gravity / 10 if should_jump else fall_gravity)
-	
+	return jump_gravity if velocity.y < 0.0 else (fall_gravity / 20 if flying else fall_gravity)
+
 func jump():
 	velocity.y = jump_velocity
-	
+
 func set_xpos(pos):
 	xpos = pos
 
